107d47f0b5349e876db734e2d3b6c399ba5af426
standardmate <standardmate7@gmail.com>
2021-10-10 21:49:45 +0200

    Update Futo.cs

--- sakk/Futo.cs
+++ sakk/Futo.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
 namespace sakk
 {
     class Futo : Babu
@@ -18,16 +19,67 @@ namespace sakk
 
         public override List<Tuple<int, int>> LehetsegesLepesek()
         {
-            List<Tuple<int, int>> lepesek = new List<Tuple<int, int>>();
+            List<Tuple<int, int>> lepesek = LehetsegesLepesek();
 
+<<<<<<< HEAD
+            //jobbra fel
+            for (int i = 1; i < 8; i++)
+            {
+
+                if (i != helyX)
+                {
+                    lepesek.Add(new Tuple<int, int>(helyX + i, helyY - i));
+                }
+                //nem kell a helyY ++?;
+                //8 ig megy a ciklus, nem indexel ki a pályáról? lónál?
+            }
+            //balra le
+            for (int i = 1; i < 8; i++)
+            {
+
+                if (i != helyY)
+                {
+                    lepesek.Add(new Tuple<int, int>(helyX - i, helyY + i));
+                }
+
+            }
+
+            //jobbra le
+            for (int i = 1; i < 8; i++)
+            {
+
+                if (i != helyX)
+                {
+                    lepesek.Add(new Tuple<int, int>(helyX + i, helyY + i));
+                }
+
+            }
+            //balra fel
+            for (int i = 1; i < 8; i++)
+            {
+
+                if (i != helyY)
+                {
+                    lepesek.Add(new Tuple<int, int>(helyX - i, helyY - i));
+                }
+
+            }
+=======
+>>>>>>> 1ddc46d8d5f3287dcae90e6cf279592cae344e9a
             return lepesek;
         }
 
         public override List<Tuple<int, int>> JoLepesek(Babu[,] tablaAllas)
         {
             List<Tuple<int, int>> lepesek = LehetsegesLepesek();
+<<<<<<< HEAD
+            
+            return lepesek;
+            
+=======
 
             return lepesek;
+>>>>>>> 1ddc46d8d5f3287dcae90e6cf279592cae344e9a
         }
 
         public override Babu Copy(Babu hova)
