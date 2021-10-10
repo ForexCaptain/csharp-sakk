cebe649a4fe25a0b80c0cbeeab45736e9abda43b
bosendorfair <66908767+bosendorfair@users.noreply.github.com>
2021-10-10 19:23:50 +0200

    Update Fajlkezeles.cs

--- sakk/Fajlkezeles.cs
+++ sakk/Fajlkezeles.cs
@@ -45,6 +45,31 @@ namespace sakk
         public static void Mentes(string fajlnev, Babu[,] allas)
         {
             //ToDo: Játék állás fájlba mentése
+            
+            StreamWriter iras = new StreamWriter($"{fajlnev}.txt", true, Encoding.UTF8); //opcionális: $"{fajlnev}_{DateTime.Now}.txt", true, Encoding.UTF8
+            int index = 0;
+
+            Console.WriteLine($"A {DateTime.Now}-i sakkállás a következő:");
+            for (int i = 0; i < 8; i++)
+			{
+                for (int j = 0; j < 8; j++)
+			    {
+                    if (allas[i,j]!=null)
+	                {
+                        iras.WriteLine(allas[i,j].Jeloles, allas[i,j], allas[i,j].Szin);
+                        index++;
+	                }
+                    else
+	                {
+                        //teszteléshez kiíratás
+                        Console.WriteLine("Itt nincs semmi!");
+                        index++;
+	                }
+                    
+			    }
+			}
+            //teszteléshez kiíratás
+            Console.WriteLine($"Index értéke: {index}");
         }
     }
 }
