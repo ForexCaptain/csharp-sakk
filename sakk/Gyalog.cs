2be261d447eda7c4bc1a589da28cf86f489e9b72
bosendorfair <66908767+bosendorfair@users.noreply.github.com>
2021-10-10 23:47:42 +0200

    Update Gyalog.cs
    Lehetséges lépések és kijönnek belőle a táblán kívül eső mezők

--- sakk/Gyalog.cs
+++ sakk/Gyalog.cs
@@ -21,42 +21,139 @@ namespace sakk
         {
            List<Tuple<int,int>> lehetsegesLepesek = new List<Tuple<int, int>>();
                        
-           if (helyY == 1 || helyY == 6)
-	       {
-               //fekete lepesek
-               lehetsegesLepesek.Add(new Tuple<int, int>(helyX, helyY+2));
-               lehetsegesLepesek.Add(new Tuple<int, int>(helyX,helyY+1));
-               lehetsegesLepesek.Add(new Tuple<int, int>(helyX-1,helyY+1));
-               lehetsegesLepesek.Add(new Tuple<int, int>(helyX+1,helyY+1));
+           //if (helyY == 1 || helyY == 6)
+	       //{
+           //    //fekete lepesek
+           //    if (helyY+2<8 || helyY-1>0 || helyX+1<8 || helyX-1>0)
+	       //    {
+           //         lehetsegesLepesek.Add(new Tuple<int, int>(helyX,helyY+2));
+           //         lehetsegesLepesek.Add(new Tuple<int, int>(helyX,helyY+1));
+           //         lehetsegesLepesek.Add(new Tuple<int, int>(helyX-1,helyY+1));
+           //         lehetsegesLepesek.Add(new Tuple<int, int>(helyX+1,helyY+1));
+	       //    }
+           //    
+           //    else if (helyY-2>0 || helyX+1<8 || helyX-1>0)
+	       //     {
+           //         //feher lepesek
+           //         lehetsegesLepesek.Add(new Tuple<int, int>(helyX, helyY-2));
+           //         lehetsegesLepesek.Add(new Tuple<int, int>(helyX,helyY-1));
+           //         lehetsegesLepesek.Add(new Tuple<int, int>(helyX+1,helyY-1));
+           //         lehetsegesLepesek.Add(new Tuple<int, int>(helyX-1,helyY-1));
+           //
+	       //     }
+           //    
+	       //}
+           //
+           //else
+	       //{
+           //     if (helyY+1<8 || helyY-1>0 || helyX+1<8 || helyX-1>0)
+	       //     {
+           //         //fekete lepesek
+           //         lehetsegesLepesek.Add(new Tuple<int, int>(helyX,helyY+1));
+           //         lehetsegesLepesek.Add(new Tuple<int, int>(helyX-1,helyY+1));
+           //         lehetsegesLepesek.Add(new Tuple<int, int>(helyX+1,helyY+1));
+	       //     }
+           //    
+           //     else if (helyY+1<8 || helyY-1>0 || helyX+1<8 || helyX-1>0)
+	       //     {
+           //         //feher lepesek
+           //         lehetsegesLepesek.Add(new Tuple<int, int>(helyX,helyY-1));
+           //         lehetsegesLepesek.Add(new Tuple<int, int>(helyX+1,helyY-1));
+           //         lehetsegesLepesek.Add(new Tuple<int, int>(helyX-1,helyY-1));
+	       //     }
+           //    
+	       //}
 
-               //feher lepesek
-               lehetsegesLepesek.Add(new Tuple<int, int>(helyX, helyY-2));
-               lehetsegesLepesek.Add(new Tuple<int, int>(helyX,helyY-1));
-               lehetsegesLepesek.Add(new Tuple<int, int>(helyX+1,helyY-1));
-               lehetsegesLepesek.Add(new Tuple<int, int>(helyX-1,helyY-1));
-	       }
+            if (Szin=="fekete")
+	        {
+                if (helyY == 1)
+	            {
+                    //fekete lepesek
+               
+                    lehetsegesLepesek.Add(new Tuple<int, int>(helyX,helyY+2));
+                    lehetsegesLepesek.Add(new Tuple<int, int>(helyX,helyY+1));
+                    lehetsegesLepesek.Add(new Tuple<int, int>(helyX-1,helyY+1));
+                    lehetsegesLepesek.Add(new Tuple<int, int>(helyX+1,helyY+1));
+	            }
+
+                else
+	            {
+                
+                    //fekete lepesek
+                    lehetsegesLepesek.Add(new Tuple<int, int>(helyX,helyY+1));
+                    lehetsegesLepesek.Add(new Tuple<int, int>(helyX-1,helyY+1));
+                    lehetsegesLepesek.Add(new Tuple<int, int>(helyX+1,helyY+1));         
+               
+	            }
+	        }
+
+            else
+	        {
+                if (helyY == 6)
+	            {
+                    //feher lepesek
+                    lehetsegesLepesek.Add(new Tuple<int, int>(helyX, helyY-2));
+                    lehetsegesLepesek.Add(new Tuple<int, int>(helyX,helyY-1));
+                    lehetsegesLepesek.Add(new Tuple<int, int>(helyX+1,helyY-1));
+                    lehetsegesLepesek.Add(new Tuple<int, int>(helyX-1,helyY-1));
+	            }
+
+                else
+	            {
+                
+                    //feher lepesek
+                    lehetsegesLepesek.Add(new Tuple<int, int>(helyX,helyY-1));
+                    lehetsegesLepesek.Add(new Tuple<int, int>(helyX+1,helyY-1));
+                    lehetsegesLepesek.Add(new Tuple<int, int>(helyX-1,helyY-1));
+
+	            }
+	        }
+            for (int i = 0; i < lehetsegesLepesek.Count; i++)
+			{
+                if (lehetsegesLepesek[i].Item1<0 || lehetsegesLepesek[i].Item2>8)
+	            {
+                    lehetsegesLepesek.Remove(lehetsegesLepesek[i]);
+	            }
+  			}
+            
            
-           else
-	       {
-               //fekete lepesek
-               lehetsegesLepesek.Add(new Tuple<int, int>(helyX,helyY+1));
-               lehetsegesLepesek.Add(new Tuple<int, int>(helyX-1,helyY+1));
-               lehetsegesLepesek.Add(new Tuple<int, int>(helyX+1,helyY+1));
-
-               //feher lepesek
-               lehetsegesLepesek.Add(new Tuple<int, int>(helyX,helyY-1));
-               lehetsegesLepesek.Add(new Tuple<int, int>(helyX+1,helyY-1));
-               lehetsegesLepesek.Add(new Tuple<int, int>(helyX-1,helyY-1));
-	       }
            
            return lehetsegesLepesek;
+           
         }
 
         public override List<Tuple<int, int>> JoLepesek(Babu[,] tablaAllas)
         {
             List<Tuple<int, int>> lepesek = LehetsegesLepesek();
+            List<Tuple<int, int>> joLepesek = new List<Tuple<int, int>>();
 
+
+
+            foreach (var item in lepesek)
+	        {
+                if (tablaAllas[item.Item1,item.Item2] == null )
+	            {
+                    joLepesek.Add(item);
+	            }
+
+                //else if (tablaAllas[item.Item1,item.Item2] != null && tablaAllas[item.Item1,item.Item2].Szin != tablaAllas[item.Item1,item.Item2].Szin)
+	            //{
+                //    joLepesek.Add(item);
+	            //}
+                
+	        }
+            Console.WriteLine(joLepesek);
+            //return joLepesek;
+            
+
+            for (int i = 0; i < lepesek.Count; i++)
+			{
+                Console.WriteLine(lepesek[i]);
+			}
+            Console.ReadKey();
+            
             return lepesek;
+            
         }
 
         public override Babu Copy(Babu hova)
