e444d82e1d054ea3f37c3889c71ceac65e44da46
bosendorfair <66908767+bosendorfair@users.noreply.github.com>
2021-10-17 11:19:29 +0200

    Update Gyalog.cs

--- sakk/Gyalog.cs
+++ sakk/Gyalog.cs
@@ -95,15 +95,11 @@ namespace sakk
 	        {
                 
 
-                if (tablaAllas[item.Item1,item.Item2] != null && tablaAllas[item.Item1,item.Item2].Szin != Szin)
+                if (tablaAllas[item.Item1,item.Item2] != null && item.Item1!=helyX && tablaAllas[item.Item1,item.Item2].Szin != Szin)
 	            {
                     joLepesek.Add(item);
 	            }
-                //
-                //else if (tablaAllas[item.Item1,item.Item2] != null && tablaAllas[item.Item1,item.Item2].Szin != Szin && item.Item1 == helyX && item.Item2 != helyY-1)
-	            //{
-                //    joLepesek.Add(item);
-	            //}
+                
 
                else if (tablaAllas[item.Item1,item.Item2] == null && item.Item1==helyX)
                     {
@@ -114,18 +110,18 @@ namespace sakk
 	        }
 
             //lépések teszteléséhez listák kiírása
-            for (int i = 0; i < lepesek.Count; i++)
-			{
-                Console.WriteLine("Lehetséges lépések: " + lepesek[i]);
-			}
-            
-            
-            for (int i = 0; i < joLepesek.Count; i++)
-			{
-                Console.WriteLine($"Jó lépések: " + joLepesek[i]);
-			}
-            Console.ReadKey();
-            return lepesek;
+            //for (int i = 0; i < lepesek.Count; i++)
+			//{
+            //    Console.WriteLine("Lehetséges lépések: " + lepesek[i]);
+			//}
+            //
+            //
+            //for (int i = 0; i < joLepesek.Count; i++)
+			//{
+            //    Console.WriteLine($"Jó lépések: " + joLepesek[i]);
+			//}
+            //Console.ReadKey();
+            return joLepesek;
             
         }
 
